627d66526b53b262a52daa6c9b99750021c973d5
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-03-07 16:42:31 +0100

    Update Astro configuration to use Vercel adapter, add .vercel to .gitignore, and update package dependencies accordingly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import node from '@astrojs/node';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
